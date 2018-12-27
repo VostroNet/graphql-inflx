e7d61856de883fbbeae917564d340c77be6f53f7
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2018-12-27 23:46:39 +1000

    setting globalkeys on mutation

--- src/mutation.js
+++ src/mutation.js
@@ -2,6 +2,8 @@
 
 import {GraphQLBoolean, GraphQLList, GraphQLInputObjectType, GraphQLString, GraphQLInt} from "graphql";
 
+import {fromGlobalId} from "graphql-relay/lib/node/node";
+
 import dateType from "@vostro/graphql-types/lib/date";
 import jsonType from "@vostro/graphql-types/lib/json";
 import floatType from "@vostro/graphql-types/lib/float";
@@ -52,7 +54,16 @@ export function createMutation(model) {
       },
     },
     async resolve(source, args, context, info) {
-      await model.createBulk(args.input, {context});
+      await model.createBulk(args.input.map((data) => {
+        return Object.keys(data).reduce((o, k) => {
+          if ((model.schema.globalKeys || []).indexOf(k) > -1) {
+            o[k] = fromGlobalId(data[k]).id;
+          } else {
+            o[k] = data[k];
+          }
+          return o;
+        }, {});
+      } ), {context});
       return true;
     },
   };
